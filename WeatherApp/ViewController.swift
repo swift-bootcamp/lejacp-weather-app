6720c7042d9939ac368b074538f2bd573ce38103
Ｗang Chia Jung <lejacp@angs-Mac.local>
2014-08-16 14:21:04 +0900

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ｗang Chia Jung on 2014/8/16.
+//  Copyright (c) 2014年 Ｗang Chia Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
